4049d92def9372c12387243428f678535d60e745
ShotaKitazawa <skitazawa1121@gmail.com>
2017-02-26 16:54:16 +0900

    anitimetable.py: now_timetable メソッドの実装

--- anitimetable.py
+++ anitimetable.py
@@ -9,16 +9,17 @@ import tweepy
 
 class AniTimeTable:
 
-    def __init__(self, time, broadcaster_list, CONSUMER_KEY="_", CONSUMER_SECRET="_", ACCESS_TOKEN="_", ACCESS_TOKEN_SECRET="_"):
+    def __init__(self, time, broadcaster_list, CONSUMER_KEY="_", CONSUMER_SECRET="_", ACCESS_TOKEN="_", ACCESS_TOKEN_SECRET="_", DB_CONNECTION="_"):
         if not isinstance(time, datetime.datetime):
             sys.stderr.write('Error: usage: class initialized error: argment type is not datetime.datetime\n')
-            sys.exit(1)
+            return
         self.time = time
         self.url = "http://cal.syoboi.jp/"
         self.broadcaster = broadcaster_list
         auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
         auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
         self.api = tweepy.API(auth)
+        self.connection = DB_CONNECTION
 
     def show_all(self):
         soup = self._return_soup()
@@ -40,7 +41,7 @@ class AniTimeTable:
                         title = program.find("a", {"class": "v3title"}).text
                         atime = program["title"]
                         weekday = self._check_weekday()
-                        print(title + "\n" + broadcaster_check + ": " + weekday + " " + atime + "\n" + "\n" + message)
+                        sys.stdout.write(title + "\n" + broadcaster_check + ": " + weekday + " " + atime + "\n" + "\n" + message + "\n")
 
     def _time_check(self, program, *time_ago):  # *time_age = [時,分]
         regex = "^([0-9]{2}):([0-9]{2})-([0-9]{2}):([0-9]{2}).*$"
@@ -58,7 +59,7 @@ class AniTimeTable:
             start_time = datetime.datetime(self.time.year, self.time.month, self.time.day + 1, start_hour, start_minute, 0)
             end_time = datetime.datetime(self.time.year, self.time.month, self.time.day + 1, end_hour, end_minute, 0)
         check_time = self.time + datetime.timedelta(hours=time_ago[0]) + datetime.timedelta(minutes=time_ago[1])
-        if start_time < check_time and check_time < end_time:
+        if start_time <= check_time and check_time < end_time:
             return True
         else:
             return False
@@ -73,7 +74,7 @@ class AniTimeTable:
         response = requests.get(self.url + "?date=" + self.time.strftime("%Y/%m/%d"))
         if response.status_code == 404:
             sys.stderr.write('Error: usage: URL page notfound.\n')
-            sys.exit(1)
+            return
         html = response.text.encode('utf-8', 'ignore')
         return BeautifulSoup(html, "lxml")
 
@@ -82,7 +83,22 @@ class AniTimeTable:
         atime = program["title"]
         weekday = self._check_weekday()
         tweet = title + "\n" + broadcaster + ": " + weekday + " " + atime + "\n" + "\n" + message
-        self.api.update_with_media(filename="{0}/.images/{1}-{2}/{3}.jpg".format(os.path.expanduser('~'), str(self.time.year), self._check_season(), self._escaping(title)), status=tweet)
+        anime_id_list = self._select_database('anime_id', 'anime', 'where title = "{0}"'.format(title))
+        for i in anime_id_list:
+            anime_id = i[0]
+            self.api.update_with_media(filename="{0}/.images/{1}.jpg".format(os.path.expanduser('~'), anime_id), status=tweet)
+            sys.stdout.write("{0} tweet.\n".format(title))
+
+    def _select_database(self, column, table, condition=""):
+        try:
+            c = self.connection.cursor()
+            c.execute('select {0} from {1} {2}'.format(column, table, condition))
+            values = c.fetchall()
+            c.close()
+            return values
+        except:
+            sys.stderr.write("Error: usage: Database cant connected\n")
+            return
 
     def _check_weekday(self):
         if self.time.weekday() == 0:
